393cf39e538770341f69fd01a50832b624d327c3
JoshMR94 <joshuamarram@gmail.com>
2022-01-26 19:55:45 +0100

    Added a small example of categories mapped and how to add a new one statically

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon ball']);
+
+    const handleAdd = () => {
+        const newCategories = [...categories, 'Doraemon'];
+        setCategories(newCategories);
+    }
+
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                {   
+                    categories.map( (category, idx) => {
+                        return <li key={ category }> { category } </li>
+                    })
+                }
+            </ol>
         </>
     );
 
